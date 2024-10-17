0a5490664b6ecb49acc8d48a3d9eeb9050e2bd8f
Kabita Shrestha <kabitashrestha0009@gmail.com>
2024-10-16 22:34:03 -0400

    Kubernetes pods and services

--- main.tf
+++ main.tf
@@ -21,19 +21,23 @@ provider "azurerm" {
 resource "azurerm_resource_group" "resource_group" {
   name     = "azure_devops_resource_group" # Name of the resource group.
   location = "Canada Central"              # Region where the resource group will be created.
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # Creates an Azure Kubernetes Service (AKS) cluster within the resource group.
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = "azure_devops_aks_cluster"  # Name of the AKS cluster.
-  location            = azurerm_resource_group.aks_rg.location  # Location is set to match the resource group.
-  resource_group_name = azurerm_resource_group.aks_rg.name      # Associates the cluster with the resource group.
-  dns_prefix          = "azure_devops_aks_cluster_dns"          # DNS prefix for the cluster.
+  name                = "azure-devops-aks-cluster"  # Name of the AKS cluster.
+  location            = azurerm_resource_group.resource_group.location  # Location is set to match the resource group.
+  resource_group_name = azurerm_resource_group.resource_group.name      # Associates the cluster with the resource group.
+  dns_prefix          = "azure-devops-aks-cluster-dns"          # DNS prefix for the cluster.
 
   # Configures the default node pool for the AKS cluster.
   default_node_pool {
-    name                = "cluster_pool"          # Name of the node pool.
-    vm_size             = "Standard_D2s_v2"       # Specifies the virtual machine size for the nodes.
+    name                = "clusterpool"          # Name of the node pool.
+    vm_size             = "Standard_B2s"       # Specifies the virtual machine size for the nodes.
     auto_scaling_enabled = true                   # Enables auto-scaling for the node pool.
     node_count          = 3                       # Initial number of nodes.
     min_count           = 1                       # Minimum number of nodes when scaling down.
